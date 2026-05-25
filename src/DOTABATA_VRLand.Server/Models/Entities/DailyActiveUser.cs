40fec5fab0077b7a2002986a022c05f18e6531ef
S-JoBi24-095\student <241211@st.yoshida-g.ac.jp>
2026-05-25 09:10:15 +0900

    デイリーアクティブユーザー登録の実装

---
+++ src/DOTABATA_VRLand.Server/Models/Entities/DailyActiveUser.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DOTABATA_VRLand.Server.Models.Entities
+{
+
+    [Table("daily_active_users")]
+    public class DailyActiveUser
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime ActivityDate { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedDay { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+
+}
