da8fb178ec30ea5da0d7085e6181459feb4117b3
S-JoBi24-095\student <241211@st.yoshida-g.ac.jp>
2026-06-11 14:14:54 +0900

    ミニゲームクラスの追加、ルーム関係の処理修正

---
+++ src/DOTABATA_VRLand.Server/Models/Entities/MiniGameLogs.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DOTABATA_VRLand.Server.Models.Entities
+{
+    [Table("miniGame_logs")]
+    public class MiniGameLogs
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+
+        [Column("miniGame_status_id")]
+        public int MiniGameStatusId { get; set; }
+
+        [Column("user_id")]
+        public int Userid { get; set; }
+
+        [Column("meinGame_log_id")]
+        public int MeinGameLogId { get; set; }//タイムアタック:1 スコアアタック:2
+
+        [Column("Score")]
+        public int Score { get; set; }//勝利数
+
+        [Column("user_rank")]
+        public int UserRank { get; set; }//順位
+
+        [Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+
+        [Column("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ src/DOTABATA_VRLand.Server/Models/Entities/RoomUser.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DOTABATA_VRLand.Server.Models.Entities
+{
+    [Table("room_users")]
+    public class RoomUser
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("room_id")]
+        public int RoomId { get; set; }
+        [Column("user_id")]
+        public int UserId { get; set; }
+    }
+}
