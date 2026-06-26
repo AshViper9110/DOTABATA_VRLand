e593cc5950cd5fdda7839cd67da1479fffe51625
Tatunoko0411 <241202@st.yoshida-g.ac.jp>
2026-06-27 08:38:52 +0900

    ボール生成されない不具合を修正、UI類の調整

--- src/DOTABATA_VRLand.Unity/Assets/Scenes/Works/otake/script/BombDodge/BomberManager.cs
+++ src/DOTABATA_VRLand.Unity/Assets/Scenes/Works/otake/script/BombDodge/BomberManager.cs
@@ -5,7 +5,7 @@ public class BomberManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Destroy(gameObject,1.0f);
     }
 
     // Update is called once per frame
