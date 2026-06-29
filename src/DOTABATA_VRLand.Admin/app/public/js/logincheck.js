8379c9724f01f03bc02ed4f59c5a1e2c27dca64d
S-JoBi24-095\student <241211@st.yoshida-g.ac.jp>
2026-06-29 09:24:57 +0900

    管理ツール管理ユーザーの処理、ページを作成

--- src/DOTABATA_VRLand.Admin/app/public/js/logincheck.js
+++ src/DOTABATA_VRLand.Admin/app/public/js/logincheck.js
@@ -4,7 +4,7 @@ async function checkLogin() {
     const data = await res.json();
 
     if (!data.loggedIn) {
-        location.href = "/main.html";
+        location.href = "/index.js";
     }
 }
 
