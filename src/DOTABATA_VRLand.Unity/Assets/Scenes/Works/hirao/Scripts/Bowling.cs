5136713d165ab5b7b2c1e1dd1ec2c4dc844f9ddd
DESKTOP-GQ60U0C\student <241210@st.yoshida-g.ac.jp>
2026-07-16 10:31:59 +0900

    ボーリングでの初期の干渉の修正

--- src/DOTABATA_VRLand.Unity/Assets/Scenes/Works/hirao/Scripts/Bowling.cs
+++ src/DOTABATA_VRLand.Unity/Assets/Scenes/Works/hirao/Scripts/Bowling.cs
@@ -25,6 +25,7 @@ public class Bowling : MonoBehaviour
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip pinDown;
     [SerializeField] private GameObject panel;
+    [SerializeField] private GameObject monitor;
     private int defeatedPinCount = 0;
     private float panelOffset = 2;
 
@@ -116,6 +117,7 @@ public class Bowling : MonoBehaviour
             DeletePins();
             RoomModel.I.SendScore(defeatedPinCount);
             await RoomModel.I.BallingNext(defeatedPinCount, InRoomPlayerData.I.PlayerList[RoomModel.I.ConnectionId].joinedUser);
+            defeatedPinCount = 0;
         }
         else if (currentNextGameTime > 0)
         {
@@ -209,6 +211,7 @@ public class Bowling : MonoBehaviour
         if (remain <= 0)
         {
             AudioManager.ChangeBGM(AudioManager.BGM.Bowling);
+            monitor.SetActive(true);
         }
     }
 }
